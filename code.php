120de7454670263201f2c2aa4e38cfd9ce21d9b5
CreepstyC <marlonbarajas07@gmail.com>
2023-02-16 19:58:27 -0500

    first commit

---
+++ code.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+require 'dbcon.php';
+
+if(isset($_POST['delete_user']))
+{
+    $user_id = mysqli_real_escape_string($con, $_POST['delete_user']);
+
+    $query = "DELETE FROM users WHERE id='$user_id' ";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run)
+    {
+        $_SESSION['message'] = "user Deleted Successfully";
+        header("Location: index.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "user Not Deleted";
+        header("Location: index.php");
+        exit(0);
+    }
+}
+
+if(isset($_POST['update_user']))
+{
+    $user_id = mysqli_real_escape_string($con, $_POST['user_id']);
+
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+    $course = mysqli_real_escape_string($con, $_POST['course']);
+
+    $query = "UPDATE users SET name='$name', email='$email', phone='$phone', course='$course' WHERE id='$user_id' ";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run)
+    {
+        $_SESSION['message'] = "user Updated Successfully";
+        header("Location: index.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "user Not Updated";
+        header("Location: index.php");
+        exit(0);
+    }
+
+}
+
+
+if(isset($_POST['save_user']))
+{
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+    $course = mysqli_real_escape_string($con, $_POST['course']);
+
+    $query = "INSERT INTO users (name,email,phone,course) VALUES ('$name','$email','$phone','$course')";
+
+    $query_run = mysqli_query($con, $query);
+    if($query_run)
+    {
+        $_SESSION['message'] = "user Created Successfully";
+        header("Location: user-create.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "user Not Created";
+        header("Location: user-create.php");
+        exit(0);
+    }
+}
+
+?>
